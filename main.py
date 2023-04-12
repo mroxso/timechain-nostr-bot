985e35d09dfa614e2ec37898ef5f11b9f17efe77
PascalR <24775431+mroxso@users.noreply.github.com>
2023-04-12 14:36:42 +0200

    fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from nostr.message_type import ClientMessageType
 from nostr.key import PrivateKey
 
 relay_manager = RelayManager()
-relay_manager.add_relay("wss://test.nostr.0x50.tech")
+relay_manager.add_relay("wss://nostr.0x50.tech")
 # relay_manager.add_relay("wss://relay.damus.io")
 relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE}) # NOTE: This disables ssl certificate verification
 time.sleep(1.25) # allow the connections to open
