0d52226e65c0e19ce2759b31dfb7ebea0f7f31b0
PascalR <24775431+mroxso@users.noreply.github.com>
2023-04-12 14:33:44 +0200

    initial code commit

---
+++ createAccount.py
@@ -0,0 +1,6 @@
+from nostr.key import PrivateKey
+
+private_key = PrivateKey()
+public_key = private_key.public_key
+print(f"Private key: {private_key.bech32()}")
+print(f"Public key: {public_key.bech32()}")
