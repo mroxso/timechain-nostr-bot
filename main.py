8c3c69b5da7ababfac3a2178c52f3ab9442ffd7c
mroxso <mail@pascalrost.de>
2023-04-23 12:55:30 +0200

    potential fix for not crashing properly

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ from nostr.relay_manager import RelayManager
 from nostr.message_type import ClientMessageType
 from nostr.key import PrivateKey
 
-relay_manager = RelayManager()
-relay_manager.add_relay("wss://nostr.0x50.tech")
-# relay_manager.add_relay("wss://relay.damus.io")
-# relay_manager.add_relay("wss://nostr.mom")
-# relay_manager.add_relay("wss://relay.nostrich.de")
-# relay_manager.add_relay("wss://relay.current.fyi")
-# relay_manager.add_relay("wss://relay.nostr.band")
-relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE}) # NOTE: This disables ssl certificate verification
-time.sleep(1.25) # allow the connections to open
+try:
+    relay_manager = RelayManager()
+    relay_manager.add_relay("wss://nostr.0x50.tech")
+    # relay_manager.add_relay("wss://relay.damus.io")
+    # relay_manager.add_relay("wss://nostr.mom")
+    # relay_manager.add_relay("wss://relay.nostrich.de")
+    # relay_manager.add_relay("wss://relay.current.fyi")
+    # relay_manager.add_relay("wss://relay.nostr.band")
+    relay_manager.open_connections({"cert_reqs": ssl.CERT_NONE}) # NOTE: This disables ssl certificate verification
+    time.sleep(1.25) # allow the connections to open
 
-env_private_key = os.environ.get("PRIVATE_KEY")
-if not env_private_key:
-    print('The environment variable "PRIVATE_KEY" is not set.')
-    exit(1)
+    env_private_key = os.environ.get("PRIVATE_KEY")
+    if not env_private_key:
+        print('The environment variable "PRIVATE_KEY" is not set.')
+        exit(1)
 
-private_key = PrivateKey(bytes.fromhex(env_private_key))
+    private_key = PrivateKey(bytes.fromhex(env_private_key))
 
-old_block_height = 0
-while True:
-    try:
+    old_block_height = 0
+    while True:
         url = "https://blockchain.info/latestblock"
         response = requests.get(url)
         data = response.json()
@@ -44,6 +44,7 @@ while True:
             # relay_manager.close_connections() # NEEDED?!
             old_block_height = block_height
         time.sleep(5)
-    except Exception as e:
-        print(e)
-        exit(1)
+except Exception as e:
+    print(e)
+    relay_manager.close_connections()
+    exit(1)
