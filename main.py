ef250a2376faa6690bbe5a807769084ed36cc3c4
mr0x50 <24775431+mroxso@users.noreply.github.com>
2025-01-09 11:28:11 +0100

    add checking latest posted message before posting again the same block height

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import requests
 import time
 import ssl
 import os
-from nostr.event import Event
+import uuid
+import json
+from nostr.event import Event, EventKind
 from nostr.relay_manager import RelayManager
 from nostr.message_type import ClientMessageType
 from nostr.key import PrivateKey
+from nostr.filter import Filter, Filters
 
 def get_relays():
     env_relays = os.getenv('RELAYS')
@@ -29,11 +32,41 @@ def get_private_key():
         exit(1)
     return PrivateKey(bytes.fromhex(env_private_key))
 
+def get_public_key():
+    private_key = get_private_key()
+    return private_key.public_key.hex()
+
 def fetch_latest_block_height():
     url = "https://blockchain.info/latestblock"
     response = requests.get(url)
     data = response.json()
     return data["height"]
+
+def fetch_latest_bot_message(relay_manager):
+    public_key = get_public_key()
+    # print("Public Key: " + public_key)
+    filters = Filters([Filter(authors=[public_key], kinds=[EventKind.TEXT_NOTE], limit=1)])
+    # generate a random subscription id
+    subscription_id = str(uuid.uuid4())
+    request = [ClientMessageType.REQUEST, subscription_id]
+    request.extend(filters.to_json_array())
+
+    relay_manager.add_subscription(subscription_id, filters)
+
+    message = json.dumps(request)
+    relay_manager.publish_message(message)
+
+    while(not relay_manager.message_pool.has_events()):
+        print("Waiting for receiving latest bot message...", end="")
+        time.sleep(1)
+    print("done.")
+    message = relay_manager.message_pool.get_event().event.content
+    # print("Received latest bot message")
+    return message
+
+def fetch_latest_bot_message_height(relay_manager):
+    message = fetch_latest_bot_message(relay_manager)
+    return int(message.split(" ")[1])
 
 def main():
     try:
@@ -41,7 +74,8 @@ def main():
         relay_manager = setup_relay_manager(relays)
         private_key = get_private_key()
 
-        old_block_height = 0
+        old_block_height = fetch_latest_bot_message_height(relay_manager=relay_manager)
+        print("Old Block Height: " + str(old_block_height))
         while True:
             try:
                 block_height = fetch_latest_block_height()
